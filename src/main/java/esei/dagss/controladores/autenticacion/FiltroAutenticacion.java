d22bf663146cce00f0a0f062f10c2c96ef71b514
ribadas <ribadas@localhost>
2013-11-24 02:30:39 +0100

    Añadido filtro de URL básico para controlar el acceso a las "partes privadas" de otros tipos de usuarios (logueados o no) mediante la escritura de URL concretas (.../medico/privado/xxxxxx.xhtml) Esquema muy limitado y dependiente de los path usados en las URLS y de las etiquetas de los tipos de usuarios. Lo ideal sería emplear el esquema de autenticación de Glassfish o la librería Spring Security

--- src/main/java/esei/dagss/controladores/autenticacion/FiltroAutenticacion.java
+++ src/main/java/esei/dagss/controladores/autenticacion/FiltroAutenticacion.java
@@ -1,13 +1,10 @@
-/*
- Proyecto Java EE, DAGSS-2013
- */
-
 package esei.dagss.controladores.autenticacion;
 
+
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.inject.Inject;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -15,217 +12,84 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 /**
- *
+ * Filtro de URL para evitar el acceso a las "zonas provadas" de otros tipos de usuarios por
+ * parte de usuarios no autenticados o usuarios autenticado de otro tipo
  * @author ribadas
  */
-@WebFilter(filterName = "FiltroAutenticacion", urlPatterns = {"/*/privado/*"})
+@WebFilter(filterName = "FiltroAutenticacion",
+        urlPatterns = {"/faces/administrador/privado/*",
+            "/faces/farmacia/privado/*",
+            "/faces/medico/privado/*",
+            "/faces/paciente/privado/*",})
 public class FiltroAutenticacion implements Filter {
-    
-    private static final boolean debug = true;
 
-    // The filter configuration object we are associated with.  If
-    // this value is null, this filter instance is not currently
-    // configured. 
-    private FilterConfig filterConfig = null;
-    
+    @Inject
+    AutenticacionControlador autenticacionControlador;
+
     public FiltroAutenticacion() {
-    }    
-    
-    private void doBeforeProcessing(ServletRequest request, ServletResponse response)
-            throws IOException, ServletException {
-        if (debug) {
-            log("FiltroAutenticacion:DoBeforeProcessing");
-        }
-
-	// Write code here to process the request and/or response before
-        // the rest of the filter chain is invoked.
-	// For example, a logging filter might log items on the request object,
-        // such as the parameters.
-	/*
-         for (Enumeration en = request.getParameterNames(); en.hasMoreElements(); ) {
-         String name = (String)en.nextElement();
-         String values[] = request.getParameterValues(name);
-         int n = values.length;
-         StringBuffer buf = new StringBuffer();
-         buf.append(name);
-         buf.append("=");
-         for(int i=0; i < n; i++) {
-         buf.append(values[i]);
-         if (i < n-1)
-         buf.append(",");
-         }
-         log(buf.toString());
-         }
-         */
-    }    
-    
-    private void doAfterProcessing(ServletRequest request, ServletResponse response)
-            throws IOException, ServletException {
-        if (debug) {
-            log("FiltroAutenticacion:DoAfterProcessing");
-        }
-
-	// Write code here to process the request and/or response after
-        // the rest of the filter chain is invoked.
-	// For example, a logging filter might log the attributes on the
-        // request object after the request has been processed. 
-	/*
-         for (Enumeration en = request.getAttributeNames(); en.hasMoreElements(); ) {
-         String name = (String)en.nextElement();
-         Object value = request.getAttribute(name);
-         log("attribute: " + name + "=" + value.toString());
-
-         }
-         */
-	// For example, a filter might append something to the response.
-	/*
-         PrintWriter respOut = new PrintWriter(response.getWriter());
-         respOut.println("<P><B>This has been appended by an intrusive filter.</B>");
-         */
     }
 
-    /**
-     *
-     * @param request The servlet request we are processing
-     * @param response The servlet response we are creating
-     * @param chain The filter chain we are processing
-     *
-     * @exception IOException if an input/output error occurs
-     * @exception ServletException if a servlet error occurs
-     */
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain)
-            throws IOException, ServletException {
-        
-        if (debug) {
-            log("FiltroAutenticacion:doFilter()");
-        }
-        
-        doBeforeProcessing(request, response);
-        
-        Throwable problem = null;
-        try {
-            chain.doFilter(request, response);
-        } catch (Throwable t) {
-	    // If an exception is thrown somewhere down the filter chain,
-            // we still want to execute our after processing, and then
-            // rethrow the problem after that.
-            problem = t;
-            t.printStackTrace();
-        }
-        
-        doAfterProcessing(request, response);
-
-	// If there was a problem, we want to rethrow it if it is
-        // a known type, otherwise log it.
-        if (problem != null) {
-            if (problem instanceof ServletException) {
-                throw (ServletException) problem;
-            }
-            if (problem instanceof IOException) {
-                throw (IOException) problem;
-            }
-            sendProcessingError(problem, response);
-        }
-    }
-
-    /**
-     * Return the filter configuration object for this filter.
-     */
-    public FilterConfig getFilterConfig() {
-        return (this.filterConfig);
-    }
-
-    /**
-     * Set the filter configuration object for this filter.
-     *
-     * @param filterConfig The filter configuration object
-     */
-    public void setFilterConfig(FilterConfig filterConfig) {
-        this.filterConfig = filterConfig;
-    }
-
-    /**
-     * Destroy method for this filter
-     */
-    public void destroy() {        
-    }
-
-    /**
-     * Init method for this filter
-     */
-    public void init(FilterConfig filterConfig) {        
-        this.filterConfig = filterConfig;
-        if (filterConfig != null) {
-            if (debug) {                
-                log("FiltroAutenticacion:Initializing filter");
-            }
-        }
-    }
-
-    /**
-     * Return a String representation of this object.
-     */
     @Override
-    public String toString() {
-        if (filterConfig == null) {
-            return ("FiltroAutenticacion()");
-        }
-        StringBuffer sb = new StringBuffer("FiltroAutenticacion(");
-        sb.append(filterConfig);
-        sb.append(")");
-        return (sb.toString());
-    }
-    
-    private void sendProcessingError(Throwable t, ServletResponse response) {
-        String stackTrace = getStackTrace(t);        
-        
-        if (stackTrace != null && !stackTrace.equals("")) {
-            try {
-                response.setContentType("text/html");
-                PrintStream ps = new PrintStream(response.getOutputStream());
-                PrintWriter pw = new PrintWriter(ps);                
-                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-                // PENDING! Localize this for next official release
-                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");                
-                pw.print(stackTrace);                
-                pw.print("</pre></body>\n</html>"); //NOI18N
-                pw.close();
-                ps.close();
-                response.getOutputStream().close();
-            } catch (Exception ex) {
-            }
+        boolean aceptarPeticion = true;
+
+        if (!autenticacionControlador.isUsuarioAutenticado()) {
+            // No se trata de un usuario autenticado
+            aceptarPeticion = false;
         } else {
-            try {
-                PrintStream ps = new PrintStream(response.getOutputStream());
-                t.printStackTrace(ps);
-                ps.close();
-                response.getOutputStream().close();
-            } catch (Exception ex) {
+            // Comprobar que el tipo de la zona web a la que se quiere acceder 
+            // se corresponde con el tipo de usuario autenticado en la aplicación
+
+            String tipoUsuarioAutenticado = autenticacionControlador.getUsuario().getTipoUsuario().getEtiqueta().toLowerCase();
+
+            // Recuperar el path de la URL y extraer el tipo
+            HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+            String tipoUsuarioURL = extraerTipoUsuarioURL(httpServletRequest.getPathInfo());
+
+            if (!tipoUsuarioAutenticado.equalsIgnoreCase(tipoUsuarioURL)) {
+                aceptarPeticion = false;
+            }
+
+            if (aceptarPeticion) {
+                // Dejar continuar la petición
+                chain.doFilter(request, response);
+            } else {
+                // Finalizar la sesión del usuario (¿demasiado extrema?)
+                httpServletRequest.getSession().invalidate();
+                
+                // TODO: loguear el intento de acceso
+                
+                // Redirir a la página de login del tipo de usuario que corresponda
+                String contextPath = httpServletRequest.getContextPath();
+                ((HttpServletResponse) response).sendRedirect(contextPath + "/faces/" + tipoUsuarioURL + "/login.xhtml");
             }
         }
     }
-    
-    public static String getStackTrace(Throwable t) {
-        String stackTrace = null;
-        try {
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            t.printStackTrace(pw);
-            pw.close();
-            sw.close();
-            stackTrace = sw.getBuffer().toString();
-        } catch (Exception ex) {
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig
+    ) {
+    }
+
+    private String extraerTipoUsuarioURL(String path) {
+        String tipoURL = "";
+
+        Pattern patron = Pattern.compile(".*/(.+)/privado/.*");
+        Matcher matcher = patron.matcher(path);
+
+        if (matcher.find()) {
+            tipoURL = matcher.group(1);
         }
-        return stackTrace;
+        return tipoURL;
     }
-    
-    public void log(String msg) {
-        filterConfig.getServletContext().log(msg);        
-    }
-    
 }
